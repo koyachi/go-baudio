e134423845984b6deafbe70b3ea26ab7f49fa991
koyachi <rtk2106@gmail.com>
2013-03-01 06:14:18 +0900

    add example/tune.go

---
+++ example/tune.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"../../go-baudio"
+	"math"
+)
+
+func main() {
+	b := baudio.New(func(t float64) float64 {
+		var flag float64
+		if math.Mod(t, 2) > 1 {
+			flag = 1
+		} else {
+			flag = 0
+		}
+		x := math.Sin(t*400*math.Pi*2) + math.Sin(t*500)*flag
+		return x
+	})
+	b.Play()
+}
